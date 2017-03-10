ee4169f1610f1044c2d9aa286961c1f4e7dfe6c3
Thierry Bugier Pineau <tbugier@teclib.com>
2017-03-10 18:02:37 +0100

    wip

--- install/mysql/plugin_flyvemdm_empty.sql
+++ install/mysql/plugin_flyvemdm_empty.sql
@@ -237,11 +237,10 @@ CREATE TABLE `glpi_plugin_flyvemdm_invitationlogs` (
 DROP TABLE IF EXISTS `glpi_plugin_flyvemdm_tasks`;
 CREATE TABLE `glpi_plugin_flyvemdm_tasks` (
   `id`                                  int(11) NOT NULL AUTO_INCREMENT,
-  `name`                                varchar(255) NOT NULL DEFAULT '',
   `date_creation`                       datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `date_mod`                            datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `plugin_flyvemdm_agents_id`           int(11) NOT NULL DEFAULT '0',
-  `plugin_flyvemdm_fleets_policies_id`  int(11) NOT NULL DEFAULT '0',
+  `plugin_flyvemdm_policies_id`         int(11) NOT NULL DEFAULT '0',
   `status`                              varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
