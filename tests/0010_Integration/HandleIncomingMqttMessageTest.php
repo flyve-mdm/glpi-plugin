b319814d8a61438f6bf3a8e0dfdc75c4beee8302
Thierry Bugier Pineau <tbugier@teclib.com>
2017-06-02 18:00:07 +0200

    chore(tests): fix unit  tests against GLPI 9.2 in development

--- tests/0010_Integration/HandleIncomingMqttMessageTest.php
+++ tests/0010_Integration/HandleIncomingMqttMessageTest.php
@@ -71,7 +71,11 @@ class HandleIncomingMqttMessageTest extends RegisteredUserTestCase
       self::setupGLPIFramework();
 
       // Login as guest user
-      $_REQUEST['user_token'] = User::getPersonalToken(self::$invitation->getField('users_id'));
+      if (version_compare(GLPI_VERSION, "9.2", "ge")) {
+         $_REQUEST['user_token']= User::getToken(self::$invitation->getField('users_id'), 'api_token');
+      } else {
+         $_REQUEST['user_token']= User::getPersonalToken(self::$invitation->getField('users_id'));
+      }
       self::login('', '', false);
       unset($_REQUEST['user_token']);
 
