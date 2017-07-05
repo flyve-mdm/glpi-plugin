5ca29f2c5711d27eb30b081c211fdd9321339932
Thierry Bugier Pineau <tbugier@teclib.com>
2017-07-06 00:05:19 +0200

    doc(license): update copyright and fix all headers

--- lib/GlpiLocalesExtension.php
+++ lib/GlpiLocalesExtension.php
@@ -1,31 +1,33 @@
 <?php
-/*
- LICENSE
-
-This file is part of the flyvemdm plugin.
-
-Order plugin is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Order plugin is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GLPI; along with flyvemdm. If not, see <http://www.gnu.org/licenses/>.
---------------------------------------------------------------------------
-@package   flyvemdm
-@author    the flyvemdm plugin team
-@copyright Copyright (c) 2015 flyvemdm plugin team
-@license   GPLv2+ http://www.gnu.org/licenses/gpl.txt
-@link      https://github.com/teclib/flyvemdm
-@link      http://www.glpi-project.org/
-@since     0.1.0
-----------------------------------------------------------------------
-*/
+/**
+ * LICENSE
+ *
+ * Copyright © 2016-2017 Teclib'
+ * Copyright © 2010-2016 by the FusionInventory Development Team.
+ *
+ * This file is part of Flyve MDM Plugin for GLPI.
+ *
+ * Flyve MDM Plugin for GLPI is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM Plugin for GLPI is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Flyve MDM Plugin for GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Flyve MDM Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
+ * ------------------------------------------------------------------------------
+ * @author    the flyvemdm plugin team
+ * @copyright Copyright © 2017 Teclib
+ * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+ * @link      https://github.com/flyve-mdm/flyve-mdm-glpi
+ * @link      https://flyve-mdm.com/
+ * ------------------------------------------------------------------------------
+ */
 
 class GlpiLocalesExtension extends \Twig_Extension
 {
