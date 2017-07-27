9c03d04f049afc42bf0ff63e35bafa4a9fbbdc61
Thierry Bugier Pineau <tbugier@teclib.com>
2017-07-27 10:20:01 +0200

    fix(api): restrict file and package download to the API only

--- inc/package.class.php
+++ inc/package.class.php
@@ -367,8 +367,9 @@ class PluginFlyvemdmPackage extends CommonDBTM {
    public function post_getFromDB() {
       // Check the user can view this itemtype and can view this item
       if ($this->canView() && $this->canViewItem()) {
-         if (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] == 'application/octet-stream'
-               || isset($_GET['alt']) && $_GET['alt'] == 'media') {
+         if (isAPI()
+             && (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] == 'application/octet-stream'
+                 || isset($_GET['alt']) && $_GET['alt'] == 'media')) {
             $this->sendFile(); // and terminate script
          }
       }
