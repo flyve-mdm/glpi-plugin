b91aea380949d0af021c593a091b2d641bc330b2
Naylin <nmedina@teclib.com>
2017-08-17 16:55:22 -0400

    docs(hook_entity_purge): fix the description

--- inc/package.class.php
+++ inc/package.class.php
@@ -582,7 +582,7 @@ class PluginFlyvemdmPackage extends CommonDBTM {
    }
 
    /**
-    * Deletes the entities 
+    * Deletes the packcages related to the entity
     * @param CommonDBTM $item
     */
    public function hook_entity_purge(CommonDBTM $item) {
