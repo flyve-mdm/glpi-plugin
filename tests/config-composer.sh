3d6f4775bff5ec991374ba156b3f2fb934eaa421
Thierry Bugier Pineau <tbugier@teclib.com>
2017-08-18 17:23:44 +0200

    chore(tests): attempty to get rid of composer issues

--- tests/config-composer.sh
+++ tests/config-composer.sh
@@ -1,6 +1,3 @@
 if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
-  mkdir ~/.composer/ -p
-  echo '{ "config": {"github-oauth":{"github.com": ' > ~/.composer/config.json
-  echo "\"$GH_OAUTH\"" >> ~/.composer/config.json
-  echo '}}}' >> ~/.composer/config.json
+  composer config -g github-oauth.github.com $GH_OAUTH
 fi
