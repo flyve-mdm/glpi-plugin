55fbcace4619e980da66d102ede72e83b04ef33b
Naylin <nmedina@teclib.com>
2017-08-25 11:06:58 -0400

    docs(publishFlyveManifest): add @return tag

--- inc/mqtthandler.class.php
+++ inc/mqtthandler.class.php
@@ -157,6 +157,10 @@ class PluginFlyvemdmMqtthandler extends \sskaje\mqtt\MessageHandler {
       }
    }
 
+   /**
+    * Publishes the current version of Flyve
+    * @return mixed $mqtt the current version
+    */
    protected function publishFlyveManifest() {
       // Don't use version from the cosntant in setup.php because the backend may upgrade while this script is running
       // thus keep in RAM in an older version
