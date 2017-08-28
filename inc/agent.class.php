edfcc4e9c6dbddbe08ed44911ca8760fe524d29c
Naylin <nmedina@teclib.com>
2017-08-28 18:10:58 -0400

    docs(logInvitationEvent): fix the summary

--- inc/agent.class.php
+++ inc/agent.class.php
@@ -1624,7 +1624,7 @@ class PluginFlyvemdmAgent extends CommonDBTM implements PluginFlyvemdmNotifiable
       }
    }
    /**
-    * Logs the invitations and events
+    * Logs invitation events
     * @param PluginFlyvemdmInvitation $invitation
     * @param string $event
     */
