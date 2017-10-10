56c6a3c91d4c2d5dd8428d8702e203edd8e038c5
Domingo Oropeza <doropeza@teclib.com>
2017-10-10 11:41:01 -0400

    refactor(database): added new indexes to schema

--- install/upgrade/update_to_2_0_0.php
+++ install/upgrade/update_to_2_0_0.php
@@ -15,7 +15,8 @@ function update_to_2_0_0(Migration $migration) {
               `date_creation`                    datetime                  NOT NULL DEFAULT '0000-00-00 00:00:00',
               `event`                            varchar(255)              NOT NULL DEFAULT '',
               PRIMARY KEY (`id`),
-              INDEX `status` (`date_creation`)
+              INDEX `plugin_flyvemdm_invitations_id` (`plugin_flyvemdm_invitations_id`),
+              INDEX `date_creation` (`date_creation`)
             ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
 
    $DB->query($query) or die("Could not create invitation logs table " . $DB->error());
@@ -38,9 +39,14 @@ function update_to_2_0_0(Migration $migration) {
                 ADD COLUMN `enroll_status` ENUM('enrolled', 'unenrolling', 'unenrolled') NOT NULL DEFAULT 'enrolled' AFTER `lock`";
       $DB->query($query) or die("Could upgrade table $table" . $DB->error());
    }
+   $migration->addKey($table, 'computers_id', 'computers_id');
+   $migration->addKey($table, 'users_id', 'users_id');
+   $migration->addKey($table, 'entities_id', 'entities_id');
 
    $table = PluginFlyvemdmPolicy::getTable();
    $migration->addField($table, 'recommended_value', 'string', ['after' => 'default_value']);
+   $migration->addKey($table, 'group', 'group');
+   $migration->addKey($table, 'plugin_flyvemdm_policycategories_id', 'plugin_flyvemdm_policycategories_id');
 
    // remove download base URL setting
    Config::deleteConfigurationValues('flyvemdm', ['deploy_base_url']);
@@ -48,18 +54,47 @@ function update_to_2_0_0(Migration $migration) {
    $migration->addField(PluginFlyvemdmAgent::getTable(), 'version', 'string', ['after' => 'name']);
 
    Config::setConfigurationValues('flyvemdm', [
-         'default_agent_url' => PLUGIN_FLYVEMDM_AGENT_DOWNLOAD_URL
+      'default_agent_url' => PLUGIN_FLYVEMDM_AGENT_DOWNLOAD_URL,
    ]);
 
    $config = Config::getConfigurationValues('flyvemdm', ['android_bugcollecctor']);
    if (!isset($config['android_bugcollecctor_url'])) {
       $config = [
-            'android_bugcollecctor_url'      => '',
-            'android_bugcollector_login'     => '',
-            'android_bugcollector_passwd'    => '',
+         'android_bugcollecctor_url' => '',
+         'android_bugcollector_login' => '',
+         'android_bugcollector_passwd' => '',
       ];
       Config::setConfigurationValues('flyvemdm', $config);
    }
 
-   $migration->addField(PluginFlyvemdmFleet::getTable(), `is_recursive`, 'bool');
+   $migration->addKey(PluginFlyvemdmEntityconfig::getTable(), 'entities_id', 'entities_id');
+   $migration->addKey(PluginFlyvemdmFile::getTable(), 'entities_id', 'entities_id');
+
+   $table = PluginFlyvemdmFleet::getTable();
+   $migration->addField($table, 'is_recursive', 'bool');
+   $migration->addKey($table, 'entities_id', 'entities_id');
+
+   $migration->addKey(PluginFlyvemdmGeolocation::getTable(), 'computers_id', 'computers_id');
+   $migration->addKey(PluginFlyvemdmMqttacl::getTable(), 'plugin_flyvemdm_mqttusers_id',
+      'plugin_flyvemdm_mqttusers_id');
+
+   $migration->addKey(PluginFlyvemdmPackage::getTable(), 'entities_id', 'entities_id');
+   $migration->addKey(PluginFlyvemdmPolicyCategory::getTable(),
+      'plugin_flyvemdm_policycategories_id', 'plugin_flyvemdm_policycategories_id');
+
+   $table = PluginFlyvemdmTask::getTable();
+   $migration->addKey($table, 'plugin_flyvemdm_fleets_id', 'plugin_flyvemdm_fleets_id');
+   $migration->addKey($table, 'plugin_flyvemdm_policies_id', 'plugin_flyvemdm_policies_id');
+
+   $table = PluginFlyvemdmInvitation::getTable();
+   $migration->addKey($table, 'users_id', 'users_id');
+   $migration->addKey($table, 'entities_id', 'entities_id');
+   $migration->addKey($table, 'documents_id', 'documents_id');
+
+   $migration->addKey(PluginFlyvemdmInvitationlog::getTable(), 'plugin_flyvemdm_invitations_id',
+      'invitations_id');
+
+   $table = PluginFlyvemdmTaskstatus::getTable();
+   $migration->addKey($table, 'agents_id', 'agents_id');
+   $migration->addKey($table, 'tasks_id', 'tasks_id');
 }
