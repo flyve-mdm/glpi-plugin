bf39e8fc8568a68177f7ae942831b3794dfccadd
Domingo Oropeza <doropeza@teclib.com>
2017-11-17 10:28:04 -0400

    test(common): added code for common functions

---
+++ tests/suite-unit/PluginFlyvemdmCommon.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * LICENSE
+ *
+ * Copyright © 2016-2017 Teclib'
+ * Copyright © 2010-2016 by the FusionInventory Development Team.
+ *
+ * This file is part of Flyve MDM Plugin for GLPI.
+ *
+ * Flyve MDM Plugin for GLPI is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM Plugin for GLPI is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Flyve MDM Plugin for GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Flyve MDM Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
+ * ------------------------------------------------------------------------------
+ * @author    Domingo Oropeza
+ * @copyright Copyright © 2017 Teclib
+ * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+ * @link      https://github.com/flyve-mdm/flyve-mdm-glpi-plugin
+ * @link      https://flyve-mdm.com/
+ * ------------------------------------------------------------------------------
+ */
+
+namespace tests\units;
+
+
+use atoum;
+
+class PluginFlyvemdmCommon extends atoum
+{
+   protected function providerConvertToGib() {
+      return [
+         'B' => ['data' => 1, 'expected' => '1.00 B'],
+         'KiB' => ['data' => 2048, 'expected' => '2.00 KiB'],
+         'MiB' => ['data' => 2097152, 'expected' => '2.00 MiB'],
+         'GiB' => ['data' => 2147483648, 'expected' => '2.00 GiB'],
+         'TiB' => ['data' => 2199023255552, 'expected' => '2.00 TiB'],
+         '1023B' => ['data' => 1023, 'expected' => '1&nbsp;023.00 B'],
+         '2.3KiB' => ['data' => 2360, 'expected' => '2.30 KiB'],
+         '13MiB' => ['data' => 13631488, 'expected' => '13.00 MiB'],
+      ];
+   }
+
+   /**
+    * @dataProvider providerConvertToGib
+    * @tags testConvertToGiB
+    */
+   public function testConvertToGiB($data, $expected) {
+      $this->given($this->newTestedInstance)
+         ->string($this->testedInstance->convertToGib($data))->isEqualTo($expected);
+   }
+
+   /**
+    * @tags testGetEnumValues
+    */
+   public function testGetEnumValues() {
+      $class = $this->testedClass->getClass();
+      $result = $class::getEnumValues('wrongTable', 'wrongField');
+      $this->given($class)->array($result)->isEmpty();
+      $result = $class::getEnumValues('glpi_plugin_flyvemdm_agents', 'mdm_type');
+      $this->given($class)->array($result)->isNotEmpty()->containsValues(['android', 'apple']);
+   }
+
+}
