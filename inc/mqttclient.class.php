98555da64f4cde1eb54f9f7d5a1aa4799352e1e7
Thierry Bugier <tbugier@teclib.com>
2017-11-23 14:23:26 +0100

    fix(mqtt): connection to backend

--- inc/mqttclient.class.php
+++ inc/mqttclient.class.php
@@ -240,7 +240,7 @@ class PluginFlyvemdmMqttclient {
       ]);
       if (empty($config['mqtt_broker_internal_address'])
           || empty($config['mqtt_broker_port']) || empty($config['mqtt_broker_tls_port'])
-          || (isset($config['mqtt_tls_for_backend']))) {
+          || (empty($config['mqtt_tls_for_backend']))) {
           Toolbox::logInFile('mqtt', 'at least one MQTT configuration setting is missing');
          return false;
       } else {
