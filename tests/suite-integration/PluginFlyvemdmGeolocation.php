e790fe2fbe6c58258b3a37597aee8ba8dc15201f
Domingo Oropeza <doropeza@teclib.com>
2017-12-05 17:00:10 -0400

    test(geolocation): updated integration for geolocation

--- tests/suite-integration/PluginFlyvemdmGeolocation.php
+++ tests/suite-integration/PluginFlyvemdmGeolocation.php
@@ -34,40 +34,35 @@ namespace tests\units;
 use Glpi\Test\CommonTestCase;
 use Computer;
 
-class PluginFlyvemdmGeolocation  extends CommonTestCase {
-
-   public function beforeTestMethod($method) {
-      $this->resetState();
-      parent::beforeTestMethod($method);
-      $this->setupGLPIFramework();
-      $this->login('glpi', 'glpi');
-   }
+class PluginFlyvemdmGeolocation extends CommonTestCase
+{
 
    /**
-    *
+    * @tags testPurgeComputer
     */
    public function testPurgeComputer() {
+      $this->login('glpi', 'glpi');
       $this->given($computer = new Computer())
-           ->and($computerData = [
-                    'name'         => 'computer',
-                    'entities_id'  => $_SESSION['glpiactive_entity'],
-                 ],
-                 $computer->add($computerData),
-                 $this->boolean($computer->isNewItem())->isFalse(),
-                 $geolocationData = [
-                    'computers_id' => $computer->getID(),
-                    'latitude'     => '1',
-                    'longitude'    => '1',
-                    'entities_id'  => $computer->getField('entities_id'),
-                 ],
-               $geolocation = $this->newTestedInstance(),
-               $geolocation->add($geolocationData))
-           ->then()
-               ->boolean($computer->isNewItem())->isFalse("Computer not created")
-               ->boolean($geolocation->isNewItem())->isFalse("Geolocation not created")
-           ->then($computer->delete(['id' => $computer->getID()], 1))
-               ->boolean($computer->getFromDB($computer->getID()))->isFalse("Computer not deleted")
-               ->boolean($geolocation->getFromDB($geolocation->getID()))->isFalse("Geolocation not deleted");
+         ->and($computerData = [
+            'name' => 'computer',
+            'entities_id' => $_SESSION['glpiactive_entity'],
+         ],
+            $computer->add($computerData),
+            $this->boolean($computer->isNewItem())->isFalse(),
+            $geolocationData = [
+               'computers_id' => $computer->getID(),
+               'latitude' => '1',
+               'longitude' => '1',
+               'entities_id' => $computer->getField('entities_id'),
+            ],
+            $geolocation = $this->newTestedInstance(),
+            $geolocation->add($geolocationData))
+         ->then()
+         ->boolean($computer->isNewItem())->isFalse("Computer not created")
+         ->boolean($geolocation->isNewItem())->isFalse("Geolocation not created")
+         ->then($computer->delete(['id' => $computer->getID()], 1))
+         ->boolean($computer->getFromDB($computer->getID()))->isFalse("Computer not deleted")
+         ->boolean($geolocation->getFromDB($geolocation->getID()))->isFalse("Geolocation not deleted");
    }
 
 }
