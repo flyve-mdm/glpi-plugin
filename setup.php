4991b7fb6889ba425e8d834f71c3f33e66fb966b
Naylin <nmedina@teclib.com>
2018-04-13 10:27:44 -0400

    feat(setup): add new constant for agent beta url
    Signed-off-by: Naylin <nmedina@teclib.com>

--- setup.php
+++ setup.php
@@ -41,6 +41,8 @@ define('PLUGIN_FLYVEMDM_ROOT', GLPI_ROOT . '/plugins/flyvemdm');
 
 define('PLUGIN_FLYVEMDM_AGENT_DOWNLOAD_URL', 'https://play.google.com/store/apps/details?id=org.flyve.mdm.agent');
 
+define('PLUGIN_FLYVEMDM_AGENT_BETA_DOWNLOAD_URL', 'https://play.google.com/apps/testing/org.flyve.mdm.agent');
+
 define('PLUGIN_FLYVEMDM_APPLE_DEVELOPER_ENTERPRISE_URL', 'https://developer.apple.com/programs/enterprise/');
 
 define('PLUGIN_FLYVEMDM_DEEPLINK', 'http://flyve.org/deeplink/');
