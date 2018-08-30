f729b96e9f4b3967dade62f181c2a054b462d138
Naylin Medina <nmedina@teclib.com>
2018-08-30 08:58:00 -0400

    test(site): ignore # in link
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/test-jekyll.sh
+++ ci/test-jekyll.sh
@@ -4,4 +4,4 @@ set -e # halt script on error
 bundle exec jekyll build
 rm -rf _site/reports
 rm -rf _site/screenshots
-bundle exec htmlproofer ./_site --allow-hash-href --assume-extension --disable-external --file-ignore ./_site/CHANGELOG.html
+bundle exec htmlproofer ./_site --allow-hash-href --url-ignore "/#.*/" --assume-extension --disable-external --file-ignore ./_site/CHANGELOG.html
