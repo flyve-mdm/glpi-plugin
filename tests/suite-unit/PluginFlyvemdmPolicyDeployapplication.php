f6d877562bac317b22beea10598305b952bc0a91
Domingo Oropeza <doropeza@teclib.com>
2018-12-20 06:30:01 -0400

    docs: update wrong license format on headers

--- tests/suite-unit/PluginFlyvemdmPolicyDeployapplication.php
+++ tests/suite-unit/PluginFlyvemdmPolicyDeployapplication.php
@@ -21,9 +21,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Flyve MDM Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
  * ------------------------------------------------------------------------------
- * @author    Domingo Oropeza
  * @copyright Copyright © 2018 Teclib
- * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+ * @license   https://www.gnu.org/licenses/agpl.txt AGPLv3+
  * @link      https://github.com/flyve-mdm/glpi-plugin
  * @link      https://flyve-mdm.com/
  * ------------------------------------------------------------------------------
